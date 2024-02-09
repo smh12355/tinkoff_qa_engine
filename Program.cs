a5e49da2ccd5ad158f64024e827982d82bbe48a9
smh12355 <krik6162@gmail.com>
2024-02-09 13:43:51 +0500

    tinkoff_tester

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using System;
+
+namespace tinkoff_test_new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder coord1 = new StringBuilder("hey12355olegmisha");
+            string[] test = Console.ReadLine().Split();
+            int[] test1;
+            test1 = Array.ConvertAll<string,int>(test, int.Parse);
+            (coord1[test1[0]-1],coord1[test1[1]-1]) = (coord1[test1[1]-1],coord1[test1[0]-1]);
+            Console.WriteLine(coord1);
+        }
+    }
+}
